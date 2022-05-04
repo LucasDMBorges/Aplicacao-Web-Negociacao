80e0ca225060d2f7c7426c99e916ce964839a054
LucasDMBorges <dev.lucasdean@gmail.com>
2022-05-04 10:22:45 -0300

    Template dinâmico

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 import { NegociaoController } from "./controllers/negociacao-controller.js";
 
+
 const controller = new NegociaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', event=>{
